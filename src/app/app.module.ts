9a9154865ea28afefba95d8f22886b5698cc308a
flavio <fdsdev@gmail.com>
2017-06-23 23:32:58 -0300

    implementacao inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {LoginViewComponent} from "./login-view/login-view.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login-view/login-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-view',
+  templateUrl: './login-view.component.html',
+  styleUrls: ['./login-view.component.scss',  '../../../node_modules/material-design-iconic-font/dist/css/material-design-iconic-font.min.css']
+})
+export class LoginViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
